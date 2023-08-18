63cbfcb03a3334c0488dffe2bced28bb6c714b43
Jonathan P. Wolfe <jpaulwolfe@gmail.com>
2023-08-18 05:54:23 -0600

    formatting only

--- renderer/main.cpp
+++ renderer/main.cpp
@@ -10,31 +10,37 @@ int main()
 	float CLEAR_GREEN;
 	float CLEAR_BLUE;
 	float CLEAR_ALPHA;
+
 	WINDOW_WIDTH = 800;
 	WINDOW_HEIGHT = 800;
 	CLEAR_RED = 0.07f;
 	CLEAR_GREEN = 0.13f;
 	CLEAR_BLUE = 0.17f;
 	CLEAR_ALPHA = 1.0f;
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-		GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "sombrosa", NULL, NULL);
-		if (window == NULL)
-		{
-			std::cout << " Failed to create GLFW window" << std::endl;
-			glfwTerminate();
-			return -1;
-		}
-		glfwMakeContextCurrent(window);
-		gladLoadGL();
-		glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-		glClearColor(CLEAR_RED, CLEAR_GREEN, CLEAR_BLUE, CLEAR_ALPHA);
-		while (!glfwWindowShouldClose(window))
-		{
-			glfwPollEvents();
-		}
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "sombrosa", NULL, NULL);
+	
+	if (window == NULL)
+	{
+		std::cout << " Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();	
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	glClearColor(CLEAR_RED, CLEAR_GREEN, CLEAR_BLUE, CLEAR_ALPHA);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
 
 	glfwTerminate();
 	return 0;
