989330493ad76572c3ddbf5e62c7f68a9fb944e3
Jonathan P. Wolfe <jpaulwolfe@gmail.com>
2023-08-18 05:41:07 -0600

    create window

--- renderer/main.cpp
+++ renderer/main.cpp
@@ -1,1 +1,26 @@
-int main() {};
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+int main() 
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		GLFWwindow* window = glfwCreateWindow(800, 600, "sombrosa", NULL, NULL);
+		if (window == NULL)
+		{
+			std::cout << " Failed to create GLFW window" << std::endl;
+			glfwTerminate();
+			return -1;
+		}
+		glfwMakeContextCurrent(window);
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+
+	glfwTerminate();
+	return 0;
+};
