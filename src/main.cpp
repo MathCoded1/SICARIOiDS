d20ed30b3b3fe1ca743f56fdd8b228757bd75836
Jonathan P. Wolfe <jpaulwolfe@gmail.com>
2023-08-20 14:27:06 -0600

    triangle drawn

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,26 @@
 
 
 #include<iostream>
-#include "./Libraries/include/glad/gl.h"
-#include "./Libraries/include/GLFW/glfw3.h"
+#include "../Libraries/include/glad/gl.h"
+#include "../Libraries/include/GLFW/glfw3.h"
+
+//Vertex Shader.
+const char* vertexShaderSource = "#version 330 core\n"
+
+"layout (location = 0) in vec3 iPos;\n"
+
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(iPos.x, iPos.y, iPos.z, 1.0);\n"
+"}\0";
+
+//Fragment Shader
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"  FragColor = vec4( 0.8f, 0.8f ,0.02f, 1.0f);\n"
+"}\n\0";
 
 // Opens a GFLW Window until terminated.  
 //	defaults used:
@@ -25,8 +43,8 @@
 int main()
 {	
 	// OpenGL specs
-	int OPENGL_VERSION_MAJOR = 4;
-	int OPENGL_VERSION_MINOR = 6;
+	int OPENGL_VERSION_MAJOR = 3;
+	int OPENGL_VERSION_MINOR = 3;
 
 	// Variables
 	const char* WINDOW_NAME;
@@ -57,12 +75,21 @@ int main()
 	);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//Create Vertices.
+	GLfloat vertices[] =
+	{
+		-0.5f,-0.5f *float(sqrt(3))/3, 0.0f,
+		0.5f,-0.5f*float(sqrt(3))/3, 0.0f,
+		0.0f,0.5f*float(sqrt(3))*2/3, 0.0f,
+		
+	};
+
 	// Create <window> Window object.
 	// terminal output of if window is created.  if not, terminates and
 	// return -1 and exit if fails (need to fix later so that it throws an error. also handle error)
 	//
 	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, NULL, NULL);
-	if (!window == NULL)
+	if (window)
 	{
 		std::cout << "Created GLFW window" << std::endl;
 	}
@@ -79,19 +106,65 @@ int main()
 	//set pixel viewport (0,0),(WINDOW_WIDTH,0),(0,WINDOW_HEIGHT),(WINDOW_WIDTH,WINDOW_HEIGHT)
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+	//Compile Vertex Shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	
+	//Compile Fragment Shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	
+	//Load Shaders into Shader Program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	//Delete Shaders
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	
+	//Create Vertex Bindings.
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+
 	// Clear color creation and then swap buffer back to paint color.
 	glClearColor(CLEAR_RED, CLEAR_GREEN, CLEAR_BLUE, CLEAR_ALPHA);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
-
+	glUseProgram(0);
 	// Keep Window open, polling.
 	// poll should know when to Exit, redraw, resize, etc.
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(CLEAR_RED, CLEAR_GREEN, CLEAR_BLUE, CLEAR_ALPHA);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//Draw
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//Flip to Back Buffer.
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-
+	//Delete Vertex arrays and Buffers.
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 	
 	//Somehow exit here.
 
@@ -99,5 +172,6 @@ int main()
 	glfwTerminate();
 
 	//return from Main()
-	return 1;
+	return 0;
 };
+
